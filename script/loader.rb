5a1e0a188011510ae845a46c5a14161db2974369
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-08-21 15:22:59 +0300

    Add loader script

---
+++ script/loader.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+require 'couchbase'
+require 'yajl'
+
+conn = Couchbase.connect(:bucket => "beer-sample")
+datadir = File.expand_path("#{File.dirname(__FILE__)}/..")
+Dir["#{datadir}/{beers,breweries}/*.json"].each do |file|
+  id = file[%r{/(.*)\.json$}, 1]
+  conn.set(id, Yajl::Parser.parse(File.read(file)))
+end
+conn.save_design_doc(File.read("#{datadir}/design_docs/beer.json"))
