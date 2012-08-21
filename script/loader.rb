0c7628ad151cb26d5226f5e135e43085bc514259
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-08-21 15:36:11 +0300

    Fix ID extraction regex

--- script/loader.rb
+++ script/loader.rb
@@ -6,7 +6,7 @@ require 'yajl'
 conn = Couchbase.connect(:bucket => "beer-sample")
 datadir = File.expand_path("#{File.dirname(__FILE__)}/..")
 Dir["#{datadir}/{beers,breweries}/*.json"].each do |file|
-  id = file[%r{/(.*)\.json$}, 1]
+  id = file[%r{/([^/]*)\.json$}, 1]
   conn.set(id, Yajl::Parser.parse(File.read(file)))
 end
 conn.save_design_doc(File.read("#{datadir}/design_docs/beer.json"))
